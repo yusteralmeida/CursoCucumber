35b26640666b468afe4698856f670958f66523d5
Yuster Almeida <yusteralmeida@gmail.com>
2021-02-21 14:06:34 -0300

    Primeiros passos no cucumber

---
+++ src/test/java/AprenderCucumber.java
@@ -0,0 +1,25 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+public class AprenderCucumber {
+
+	@Dado("^que criei o arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Quando("^executá-lo$")
+	public void executá_lo() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Então("^a especificação deve finalizar com sucesso$")
+	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
