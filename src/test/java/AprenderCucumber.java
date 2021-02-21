862d15ba8c75d2d3b382bc1c63debf849cf09e95
Yuster Almeida <yusteralmeida@gmail.com>
2021-02-21 14:43:23 -0300

    Exclusão de códigos

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,4 +1,3 @@
-import cucumber.api.PendingException;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
@@ -7,19 +6,14 @@ public class AprenderCucumber {
 
 	@Dado("^que criei o arquivo corretamente$")
 	public void que_criei_o_arquivo_corretamente() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+
 	}
 
 	@Quando("^executá-lo$")
 	public void executá_lo() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 
 	@Então("^a especificação deve finalizar com sucesso$")
 	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 }
