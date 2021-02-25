309ac8be7077b51f02504fc9bbba41972f419e4e
Yuster Almeida <yusteralmeida@gmail.com>
2021-02-25 01:57:10 -0300

    Criação de 3 cenários e seus respectivos métodos

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -74,4 +74,30 @@ public class AprenderCucumber {
 		assertEquals(data, dataFormatada);
 	}
 
+//	2 Cenários
+//	Se ouver ele no meio, entao eh usado o especial 
+	@Dado("^que o ticket( especial)? é (A.\\d{3})$")
+	public void queOTicketÉAF(String tipo, String arg1) throws Throwable {
+	}
+
+	@Dado("^que o valor da passagem é R\\$ (\\d+),(\\d+)$")
+	public void queOValorDaPassagemÉR$(int arg1, int arg2) throws Throwable {
+	}
+	
+//	Eh usado nome com minimo 5 e maximo 20 caracteres. Não pode {5, 20}
+	@Dado("^que o nome do passageiro é \"(.{5,20})\"$")// aceitar qualquer coisa
+	public void queONomeDoPassageiroÉ(String arg1) throws Throwable {
+	}
+
+	@Dado("^que o telefone do passageiro é (9\\d{3}-\\d{4})$")
+	public void queOTelefoneDoPassageiroÉ(String telefone) throws Throwable {
+	}
+
+	@Quando("^criar os steps$")
+	public void criarOsSteps() throws Throwable {
+	}
+
+	@Então("^o teste vai funcionar$")
+	public void oTesteVaiFuncionar() throws Throwable {
+	}
 }
--- src/test/java/aprender_cucumber.feature
+++ src/test/java/aprender_cucumber.feature
@@ -37,5 +37,20 @@ Cenário: Deve criar steps genéricos para estes passos
     Quando criar os steps
     Então o teste vai funcionar
 
+Cenário: Deve reaproveitar os steps "Dado" do cenário anterior
+    Dado que o ticket é AB167
+    Dado que o ticket especial é AB167
+    Dado que o valor da passagem é R$ 1120,23
+    Dado que o nome do passageiro é "Cicrano de Oliveira"
+    Dado que o telefone do passageiro é 9888-8888
+
+#Cenário: Deve negar todos os steps "Dado" dos cenários anteriores
+    #Dado que o ticket é CD123
+    #Dado que o ticket é AG1234
+    #Dado que o valor da passagem é R$ 1.1345,56
+    #Dado que o nome do passageiro é "Beltrano Souza Matos de Alcântara Azevedo"
+    #Dado que o telefone do passageiro é 1234-5678
+    #Dado que o telefone do passageiro é 999-2223 
+
 #Tipos de respostas que serão mostradas no terminal, depois de executar os testes  
 #Passou, falhou, skipped, pending e undefined 
